72a8c09756fc86acbbc864fa8540986ad0fd7a8f
Ha Nam <namnguyendangha@gmail.com>
2020-07-13 09:43:11 +0700

    history update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var mqtt = require('mqtt');
 var bodyparser = require('body-parser');
 var multer = require('multer');
 var { schedule_Post, schedule_Put, schedule_Delete, schedule_Get } = require('./database/schedule.js');
+var {history_Get, history_Post} = require('./database/history.js');
 var task = null;
 
 var app = express();
@@ -76,6 +77,17 @@ app.get('/temphumi', (req, res) => {
 //View history
 app.get('/history', (req, res) => {
     res.sendFile(__dirname + '/views/history.html');
+});
+
+app.post('/scheduling/post', (req, res) => {
+    let { hour, area, light, humidity, amount } = req.body;
+    schedule_Post({
+        hour: hour,
+        area: parseInt(area),
+        light: light,
+        humidity: humidity,
+        amount: amount,
+    }, res);
 });
 
 // var io = require('socket.io')(webserver)
--- public/script/mainHistory.js
+++ public/script/mainHistory.js
@@ -3,3 +3,18 @@
     "use strict";
     
 })(jQuery);
+
+function displayAppointment(hour, area, light, humidity, amount) {
+    let $appointment = $('#mold').clone();
+    $appointment.css('display', '');
+    $appointment.find('#hour').text(hour);
+    $appointment.find('#area').text(area);
+    $appointment.find('#light').text(light);
+    $appointment.find('#humidity').text(humidity);
+    $appointment.find('#amount').text(amount);
+    $appointment.find('#remove').click(function() {
+        if ($focusAppointment === $appointment) {
+            toggleForm();
+            $focusAppointment = null;
+        }
+    });
